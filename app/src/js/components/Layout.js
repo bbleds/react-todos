d1f953b808671329836e91737559fd29e208f586
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-04-01 14:36:13 -0500

    Removed unneeded comments

--- app/src/js/components/Layout.js
+++ app/src/js/components/Layout.js
@@ -1,30 +1,3 @@
-// import React from "react";
-// import $ from "jquery";
-//
-// // pages/components
-// import Header from "./pages/Header";
-// import Main from "./pages/Main";
-//
-//
-// export default class Layout extends React.Component {
-//   componentDidMount() {
-//     this.serverRequest = $.get("http://jsonplaceholder.typicode.com/posts/1",function(data){
-//       console.log(this);
-//     })
-//   }
-//
-//   render(){
-//   // get data
-//     return(
-//       <div>
-//         <Header />
-//         <div id="spacer"></div>
-//         <Main />
-//       </div>
-//     );
-//   }
-// }
-
 import React from "react";
 // // pages/components
 import Header from "./pages/Header";
