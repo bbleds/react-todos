bd6669f20b93cab6e3ba0ca2ee91580da75f133d
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-04-02 11:50:45 -0500

    Updating package.json, and removing unneeded webpack config items

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,6 @@
 const debug = process.env.NODE_ENV !== "production";
 const webpack = require('webpack');
 
-// module.exports = {
-//   context: __dirname+"/app/src",
-//   devtool: debug ? "inline-sourcemap": null,
-//   // this is where we start off
-//   entry: "./js/app.js",
-//   module: {
-//     loaders: [
-//       {
-//         exclude: [/bower_components/, /node_modules/, /lib/],
-//         loader: 'babel-loader',
-//         query: {
-//           presets: ['react', 'es2015', 'stage-0'],
-//           plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
-//         }
-//       }
-//     ]
-//   },
-//   output: {
-//     path: __dirname + "/app/src/",
-//     filename: "app.min.js"
-//   }
-//   plugins: [
-//     new webpack.optimize.DedupePlugin(),
-//     new webpack.optimize.OccurenceOrderPlugin(),
-//     new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
-//   ]
-//
-// };
-
 const ignore = new webpack.IgnorePlugin(/^(react|jquery|lodash|react-dom)$/);
 module.exports = {
   entry: {
